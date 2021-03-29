0917c20b0b43c5e411c45461e00387544d9545bf
DonConteo <dmitriy.tsoy.russia@gmail.com>
2021-03-30 01:28:40 +0300

    gd

--- src/main/java/com/tsoy/restApplication/controller/PersonController.java
+++ src/main/java/com/tsoy/restApplication/controller/PersonController.java
@@ -1,41 +1,52 @@
 package com.tsoy.restApplication.controller;
 
-import com.tsoy.restApplication.Person;
+import com.tsoy.restApplication.Models.Person;
+import com.tsoy.restApplication.Models.PhoneBook;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/")
 public class PersonController {
 
-    public List<Map<String, String>> phones1 = new ArrayList<Map<String, String>>(){{
-        add(new HashMap<String, String>() {{ put("number", "890"); put("contact", "kotik"); }});
-        add(new HashMap<String, String>() {{ put("number", "8901"); put("contact", "doggo"); }});
-        add(new HashMap<String, String>() {{ put("number", "8902"); put("contact", "parrot"); }});
-    }};
+    PhoneBook phoneBook1 = new PhoneBook(1, "123", "doggo1");
+    PhoneBook phoneBook2 = new PhoneBook(2, "1234", "doggo2");
+    PhoneBook phoneBook3 = new PhoneBook(3, "12345", "doggo3");
+    PhoneBook phoneBook4 = new PhoneBook(4, "123456", "doggo4");
+    PhoneBook phoneBook5 = new PhoneBook(5, "1234567", "doggo5");
+    PhoneBook phoneBook6 = new PhoneBook(6, "12345678", "doggo6");
 
-    public List<Map<String, String>> phones2 = new ArrayList<Map<String, String>>(){{
-        add(new HashMap<String, String>() {{ put("number", "8901"); put("contact", "koti"); }});
-        add(new HashMap<String, String>() {{ put("number", "89011"); put("contact", "dogg"); }});
-        add(new HashMap<String, String>() {{ put("number", "89021"); put("contact", "parro"); }});
-    }};
+    Person person1 = new Person(1, "Vasya", new ArrayList<>(){{
+        add(phoneBook1);
+        add(phoneBook2);
+        add(phoneBook3);
+    }});
+    Person person2 = new Person(2, "Petya", new ArrayList<>(){{
+        add(phoneBook4);
+        add(phoneBook5);
+        add(phoneBook6);
+    }});
 
-    Person person1 = new Person(1, "john", phones1);
-    Person person2 = new Person(2, "kuk", phones2);
-
-    public List<Person> persons = new ArrayList<Person>(){{
+    public List<Person> persons = new ArrayList<>(){{
         add(person1);
         add(person2);
     }};
 
-    @GetMapping
+    public List<PhoneBook> phoneBooks = new ArrayList<>(){{
+        add(phoneBook1);
+        add(phoneBook2);
+        add(phoneBook3);
+        add(phoneBook4);
+        add(phoneBook5);
+        add(phoneBook6);
+    }};
+
+    @GetMapping("/persons")
     public List<Person> persons(){
         return persons;
     }
@@ -52,6 +63,22 @@ public class PersonController {
     public Person getByName(@PathVariable(value = "name") String name){
         return persons.stream()
                 .filter(person -> person.getName().equals(name))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    @GetMapping("/contactId/{contactId}")
+    public PhoneBook getByContactId(@PathVariable(value = "contactId") long contactId){
+        return phoneBooks.stream()
+                .filter(phonebook -> phonebook.getContactId() == contactId)
+                .findFirst()
+                .orElseThrow();
+    }
+
+    @GetMapping("/number/{number}")
+    public PhoneBook getByNumber(@PathVariable(value = "number") String number){
+        return phoneBooks.stream()
+                .filter(phonebook -> phonebook.getNumber().equals(number))
                 .findFirst()
                 .orElseThrow();
     }
--- src/main/java/com/tsoy/restApplication/controller/PhoneBookController.java
+++ src/main/java/com/tsoy/restApplication/controller/PhoneBookController.java
@@ -14,30 +14,4 @@ import java.util.Map;
 @RequestMapping("/phones")
 public class PhoneBookController {
 
-//    public List<Map<String, String>> phones = new ArrayList<Map<String, String>>(){{
-//        add(new HashMap<String, String>() {{ put("number", "890"); put("contact", "kotik"); }});
-//        add(new HashMap<String, String>() {{ put("number", "8901"); put("contact", "doggo"); }});
-//        add(new HashMap<String, String>() {{ put("number", "8902"); put("contact", "parrot"); }});
-//    }};
-
-    @GetMapping
-    public List<Map<String, String>> list(){
-        return phones;
-    }
-
-    @GetMapping("/contact/{contact}")
-    public Map<String, String> getByContact(@PathVariable(value = "contact") String contact){
-        return phones.stream()
-                .filter(note -> note.get("contact").equals(contact))
-                .findFirst()
-                .orElseThrow();
-    }
-
-    @GetMapping("/number/{number}")
-    public Map<String, String> getByNumber(@PathVariable(value = "number") String number){
-        return phones.stream()
-                .filter(note -> note.get("number").equals(number))
-                .findFirst()
-                .orElseThrow();
-    }
 }
